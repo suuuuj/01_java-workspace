b9ab52500688f3e37e2606ad3859838b599d9a25
suuuuj <115604669+suuuuj@users.noreply.github.com>
2022-10-14 01:45:16 +0900

    복습프로젝트 생성
    10/13
    메소드출력 부분까지 복습
    (컴퓨터로 실행 한번 더 해보기)

---
+++ Review/src/com/br/first/A_MethodPrinter.java
@@ -0,0 +1,5 @@
+package com.br.first;
+
+public class A_MethodPrinter {
+
+}
---
+++ Review/src/com/br/first/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.br.first;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World~!");
+		System.out.println("안녕\n하세요.");
+		System.out.println("\"여러분~!\t건강하세요!\"");
+	}
+
+}
